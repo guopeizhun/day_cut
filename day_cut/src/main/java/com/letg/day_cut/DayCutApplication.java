d39b93fe85fa54023779c9c8b3197af08ff68756
guopeizhun <510191962@qq.com>
2023-04-25 11:25:37 +0800

    初始化

---
+++ day_cut/src/main/java/com/letg/day_cut/DayCutApplication.java
@@ -0,0 +1,13 @@
+package com.letg.day_cut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayCutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayCutApplication.class, args);
+    }
+
+}
